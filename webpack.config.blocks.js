edc8198e07b7a08b5b02f2a5a34045ddb2e67d96
Dustin Townsend <dustin.townsend@gmail.com>
2016-12-09 14:06:52 -0500

    first look at webpack blocks.

---
+++ webpack.config.blocks.js
@@ -0,0 +1,50 @@
+import path from 'path';
+
+const {
+  addPlugins, createConfig, entryPoint, env, setOutput, sourceMaps, webpack
+} = require('@webpack-blocks/webpack');
+
+const babel = require('@webpack-blocks/babel6');
+const cssModules = require('@webpack-blocks/css-modules');
+const devServer = require('@webpack-blocks/dev-server');
+const extractText = require('@webpack-blocks/extract-text');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const basePlugins = [
+  new HtmlWebpackPlugin({
+    filename: '../app.html',
+    template: 'app/app.html',
+    inject: false
+  }),
+  new webpack.DefinePlugin({
+    'process.env': JSON.stringify(process.env || 'development')
+  }),
+];
+
+const productionPlugins = [
+  new webpack.optimize.OccurrenceOrderPlugin(),
+  new webpack.optimize.DedupePlugin(),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: { warnings: true },
+    screwIe8: true
+  })
+];
+
+module.exports = createConfig([
+  entryPoint(['babel-polyfill', './app/index']),
+  setOutput(path.join(__dirname, 'app/dist')),
+  babel(),
+  cssModules(),
+  addPlugins(basePlugins),
+  env('development', [
+    sourceMaps(),
+    devServer(),
+    devServer.proxy({
+      '/dist/*': { target: 'http://localhost:3000' }
+    })
+  ]),
+  env('production', [
+    extractText(),
+    addPlugins(productionPlugins)
+  ])
+]);
